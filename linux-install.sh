da0ab0069557e43e0c1668edd958b077ac448bd2
lhchavez <lhchavez@lhchavez.com>
2017-10-28 20:01:37 -0700

    Fully support xenial
    Previous attempts to support xenial were done with an in-place upgrade,
    which had some Puppet dependencies pre-installed. This change makes it
    possible to perform a clean install of xenial.

--- linux-install.sh
+++ linux-install.sh
@@ -1,8 +1,30 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -e
 
 # Install dependencies.
-[ "`dpkg-query -W -f='${db:Status-Abbrev}' git puppet-common`" == "ii ii " ] || apt install -y -q git puppet-common
-[ "`gem list -i r10k`" == "true" ] || gem install -q r10k
+install_packages () {
+	local package_names=()
+
+	while [[ $# -gt 1 ]]; do
+		local package_name="$1"
+		shift 1
+		[ "`dpkg-query -W -f='${db:Status-Abbrev}' \"${package_name}\"`" == "ii " ] || package_names+=("${package_name}")
+	done
+	[[ ${#package_names[@]} -gt 0 ]] || return 0
+
+	apt update --assume-yes --quiet
+	apt install --assume-yes --quiet ${package_names[*]}
+}
+
+install_r10k () {
+	[[ "`gem list -i r10k`" == "true" ]] && return 0
+
+	gem install -q r10k
+}
+
+install_packages puppet-common ruby git
+install_r10k
 (cd /vagrant && r10k puppetfile install)
 
 # Install everything using Puppet
