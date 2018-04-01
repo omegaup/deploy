058deb611cdf909131c03db519e88b1d7192aa74
lhchavez <lhchavez@lhchavez.com>
2018-03-31 19:03:09 -0700

    Use sudo in linux-install.sh

--- linux-install.sh
+++ linux-install.sh
@@ -13,18 +13,18 @@ install_packages () {
 	done
 	[[ ${#package_names[@]} -gt 0 ]] || return 0
 
-	apt update --assume-yes --quiet
-	apt install --assume-yes --quiet ${package_names[*]}
+	sudo apt update --assume-yes --quiet
+	sudo apt install --assume-yes --quiet ${package_names[*]}
 }
 
 install_r10k () {
 	[[ "`gem list -i r10k`" == "true" ]] && return 0
 
-	gem install -q r10k
+	sudo gem install -q r10k
 }
 
 install_packages puppet-common ruby git
 install_r10k
-(cd /vagrant && r10k puppetfile install)
+(cd /vagrant && sudo r10k puppetfile install)
 
-puppet apply --modulepath=/etc/puppet/modules /etc/puppet/modules/omegaup/manifests/vagrant.pp
+sudo puppet apply --modulepath=/etc/puppet/modules /etc/puppet/modules/omegaup/manifests/vagrant.pp
